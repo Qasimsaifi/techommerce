34857399a71df5371481393fcde04db2395bc55e
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-08-06 16:21:30 +0530

    done

--- src/components/ThemesCollection.jsx
+++ src/components/ThemesCollection.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+"use client"
+import React, { useEffect, useRef } from 'react';
+import ScrollReveal from 'scrollreveal';
 
 const ThemesCollectionComponent = ({ themes }) => {
+  const cardRefs = useRef([]);
+
+  useEffect(() => {
+    const sr = ScrollReveal();
+
+    cardRefs.current.forEach((cardRef) => {
+      sr.reveal(cardRef, {
+        duration: 600,
+        delay: 200,
+        distance: '20px',
+        easing: 'ease-out',
+        origin: 'bottom',
+      });
+    });
+  }, []);
+
   return (
     <div className="m-5">
       <h2 className="text-2xl font-semibold text-center mt-7 mb-7">THEMES</h2>
@@ -8,9 +26,10 @@ const ThemesCollectionComponent = ({ themes }) => {
         {themes.map((theme, index) => (
           <div
             className={`card cursor-pointer relative overflow-hidden ${
-              index === 1 ? "md:col-span-2 md:row-span-2" : ""
+              index === 1 ? 'md:col-span-2 md:row-span-2' : ''
             } transition-transform duration-300 hover:scale-105`}
             key={theme.id}
+            ref={(el) => (cardRefs.current[index] = el)}
           >
             <figure className="relative">
               <img
